bf8baa2dbae4721fa9cdf36550016caf320914e4
devadaviddd <s3927112@rmit.edu.vn>
2023-07-16 01:21:14 +0700

    add supabase img & get types

--- supabase.ts
+++ supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@/types/db.types";
 
 const superbaseUrl = process.env.SUPABASE_URL!;
 const superbaseKey = process.env.SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   superbaseUrl,
   superbaseKey
 );
